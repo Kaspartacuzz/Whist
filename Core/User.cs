a673e50663d62df80124ce2730b02046f58637ae
Kaspartacuzz <eaa24kassj@students.eaaa.dk>
2025-06-26 20:51:42 +0200

    user/medlemmer

--- Core/User.cs
+++ Core/User.cs
@@ -9,6 +9,9 @@ public class User
     public string Email { get; set; } = "";                                 // Brugerens e-mailadresse
     public string Password { get; set; } = "";                              // Brugerens adgangskode (skal senere hashes i rigtig database)
     public bool IsAdmin { get; set; } = false;      
-    public string PhoneNumber { get; set; } = "";
+    public string PhoneNumber { get; set; } = "";                           // Brugerens telefonnummer - bruges til mobilepay
+    public string Description { get; set; } = "";                           // Lang tekst om personen
+    public string FunFact { get; set; } = "";                               // Sjov info
+    public string ImageUrl { get; set; } = "";                              // Profilbillede
     public ICollection<Fine> Fines { get; set; } = new List<Fine>();        // Liste over bøder
 }
