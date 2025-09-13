0977df62a8ac74ea2d9ea0d4b08a613a9ececc25
Kaspartacuzz <eaa24kassj@students.eaaa.dk>
2025-09-13 13:48:05 +0200

    email notifikationer

--- Core/Calendar.cs
+++ Core/Calendar.cs
@@ -5,4 +5,5 @@ public class Calendar
     public int Id { get; set; }
     public DateTime Date { get; set; }
     public string Note { get; set; } = "";
+    public bool ReminderSent { get; set; } = false;
 }
